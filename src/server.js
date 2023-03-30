1b36cce4a875e793981639065c0e02a9f44b2cdf
Kallen <kelvinkur22@gmail.com>
2023-03-30 12:43:54 +0700

    changed log message

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const init = async () => {
 
   server.route(routes);
   await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server runs on ${server.info.uri}`);
 };
 
 init();
